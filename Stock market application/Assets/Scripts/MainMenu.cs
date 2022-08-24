53ecc3f43f525b3542c5eed678b152004b4163e3
Joshua Leaper <poshernater@outlook.com>
2022-08-24 22:30:39 +0930

    Added login in page update

---
+++ Stock market application/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,65 @@
+using TMPro;
+using UnityEngine;
+
+// ReSharper disable IdentifierTypo
+
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Login")] 
+    public TMP_InputField logUsername;
+    public TMP_InputField logPincode;
+
+    [Header("Register")] 
+    public TMP_InputField refName;
+    public TMP_InputField refUsername;
+    public TMP_InputField regPincode;
+
+    [Header("Other")] 
+    public TMP_Text error;
+    public bool needLogin;
+
+    public void Register()
+    {
+        if (!needLogin)
+            NextScene();
+        try
+        {
+            var tName = refName.text.ToUpper();
+            var tUsername = refUsername.text.ToUpper();
+            var tPincode = regPincode.text.ToUpper();
+            MongoDB.CommitRegister(tName, tUsername, tPincode);
+        }
+        catch
+        {
+            ErrorText("Empty/invaid name, username or pin code");
+        }
+    }
+
+
+    public void Login()
+    {
+        if (!needLogin)
+            NextScene();
+        var tUsername = logUsername.text.ToUpper();
+        var tPincode = logPincode.text.ToUpper();
+        if (string.IsNullOrWhiteSpace(tUsername) || string.IsNullOrWhiteSpace(tPincode))
+        {
+            ErrorText("Empty/invaid username or pin code");
+            return;
+        }
+           
+        MongoDB.CheckLogin(tUsername,tPincode);
+    }
+    private void ErrorText(string displaytext)
+    {
+        error.text = displaytext;
+    }
+
+    private static void NextScene()
+    {
+        
+    }
+
+
+}
---
+++ Stock market application/Assets/Scripts/MongoDB.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class MongoDB : MonoBehaviour
+{
+  
+  
+    public static void CommitRegister(string name, string userName, string passCode)
+    {
+        
+    }
+    
+    public static void CheckLogin( string userName, string passCode)
+    {
+        
+    }
+}
+
